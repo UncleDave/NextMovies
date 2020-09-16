025e2fdec808c179cb1b9ee8d7981c07707498e8
Dave Pringle <3244206+UncleDave@users.noreply.github.com>
2020-09-16 19:26:07 +0100

    Add dashboard skeleton

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,54 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import movieStyles from '../styles/Movie.module.css';
+import movieListStyles from '../styles/MovieList.module.css';
 
-export default function Home() {
+function Movie() {
   return (
-    <div className={ styles.container }>
+    <div className={ movieStyles.container }>
+      <div className={ movieStyles.poster }>
+        <img src="https://m.media-amazon.com/images/M/MV5BMTk5MTI3NjI5OV5BMl5BanBnXkFtZTcwMDc5MTk1OA@@._V1_SX300.jpg" alt="Movie Poster"/>
+      </div>
+      <div className={ movieStyles.content }>
+        <h3 className={ movieStyles.title }>Title</h3>
+
+        <p className={ movieStyles.description }>Description</p>
+      </div>
+    </div>
+  );
+}
+
+function MovieList() {
+  return (
+    <ul className={ movieListStyles.container }>
+      <li className={ movieListStyles.item }>
+        <Movie/>
+      </li>
+      <li className={ movieListStyles.item }>
+        <Movie/>
+      </li>
+      <li className={ movieListStyles.item }>
+        <Movie/>
+      </li>
+      <li className={ movieListStyles.item }>
+        <Movie/>
+      </li>
+    </ul>
+  );
+}
+
+export default function Dashboard() {
+  return (
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Next Movies</title>
         <link rel="icon" href="/favicon.ico"/>
       </Head>
 
-      <main className={ styles.main }>
-        <h1 className={ styles.title }>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main>
+        <h1 style={ { textAlign: 'center' } }>Next Movies</h1>
 
-        <p className={ styles.description }>
-          Get started by editing{ ' ' }
-          <code className={ styles.code }>pages/index.js</code>
-        </p>
-
-        <div className={ styles.grid }>
-          <a href="https://nextjs.org/docs" className={ styles.card }>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={ styles.card }>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={ styles.card }
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={ styles.card }
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <MovieList/>
       </main>
-
-      <footer className={ styles.footer }>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{ ' ' }
-          <img src="/vercel.svg" alt="Vercel Logo" className={ styles.logo }/>
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
